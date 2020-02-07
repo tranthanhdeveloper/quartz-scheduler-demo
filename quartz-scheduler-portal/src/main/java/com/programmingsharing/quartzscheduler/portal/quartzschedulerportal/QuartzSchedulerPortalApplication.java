d686876d7752d3113b687c4cc99c0ded9146bf80
Tran Thanh <tranthanhdeveloper@gmail.com>
2020-02-07 21:52:09 +0700

    Initial quartz-sheduler-portal

---
+++ quartz-scheduler-portal/src/main/java/com/programmingsharing/quartzscheduler/portal/quartzschedulerportal/QuartzSchedulerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.programmingsharing.quartzscheduler.portal.quartzschedulerportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzSchedulerPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzSchedulerPortalApplication.class, args);
+    }
+
+}
---
+++ quartz-scheduler-portal/src/test/java/com/programmingsharing/quartzscheduler/portal/quartzschedulerportal/QuartzSchedulerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingsharing.quartzscheduler.portal.quartzschedulerportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzSchedulerPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
